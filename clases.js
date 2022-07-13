6c51d0c0b1052344bf67e73e57213390ed4bbebe
nikkocai <nicolas_salvanescki@hotmail.com>
2022-07-13 17:38:59 -0300

    el inicio del cambio

---
+++ clases.js
@@ -0,0 +1,19 @@
+class Producto {
+
+}
+
+class Pizza extends Producto {
+
+}
+
+class Empanada extends Producto {
+
+}
+
+class Bebida extends Producto {
+
+}
+
+class EmpanadaCarneSuave extends Empanada {
+    
+}
